0b5bbc5c332b83255ca3333ba0207be8e7018623
Clinton Sheppard <gar3ts@gmail.com>
2012-05-11 01:57:46 -0500

    don't loop, just mod if necessary and return

--- random.go
+++ random.go
@@ -8,49 +8,41 @@ type Random struct {
 	nextFeed chan int
 }
 
-func init() {
-	
-}
-
 func NewRandom() *Random {
 	r := new(Random)
 	bytes := make([]byte, 4)
-	r.nextFeed = make(chan int,100)
+	r.nextFeed = make(chan int, 100)
 	go func() {
 		for {
-			count,err := rand.Read(bytes)
-			
+			count, err := rand.Read(bytes)
+
 			if err != nil {
 				panic(err)
 			}
 			if count != 4 {
 				continue
 			}
-			
+
 			value := 0
 			multiplier := 1
-			for i:= len(bytes)-1; i >= 0; i-- {
-				value += int(bytes[i])*multiplier
+			for i := len(bytes) - 1; i >= 0; i-- {
+				value += int(bytes[i]) * multiplier
 				multiplier *= 256
 			}
 			r.nextFeed <- value
 		}
 	}()
-	
+
 	return r
 }
 
-
-func (r * Random) Intn(exclusiveMax int) int {
-	value := 0
-	for {
-		value = <- r.nextFeed
-		if value < 0 {
-			value = -value
-		}
-		if value < exclusiveMax {
-			break
-		}
+func (r *Random) Intn(exclusiveMax int) int {
+	value := <-r.nextFeed
+	if value < 0 {
+		value = -value
+	}
+	if value >= exclusiveMax {
+		value %= exclusiveMax
 	}
 	return value
 }
